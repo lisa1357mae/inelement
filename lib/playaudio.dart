d58e84594d81d2a4feea79a66d389d85a5ba47dc
Lisa Mae <lisamae135b7@me.com>
2024-07-06 13:53:46 -0400

    chore: add audio playback and audioplayers lib

---
+++ lib/playaudio.dart
@@ -0,0 +1,23 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class PlayAudio extends StatefulWidget {
+  const PlayAudio({Key? key}) : super(key: key);
+
+  @override
+  _PlayAudioState createState() => _PlayAudioState();
+}
+
+class _PlayAudioState extends State<PlayAudio> {
+  final player = AudioPlayer();
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: () {}, child: Text("play audio"));
+  }
+
+  Future<void> playSound() async {
+    String audioPath = "audio/sound.mp3";
+    // await player.play(AssetSource(audioPath));
+    await player.play(UrlSource('https://example.com/my-audio.wav'));
+  }
+}
